fe2e02b296446f4c884204ac92e44082dc28d78c
RAULPC\rgr_1 <rgr182>
2024-10-04 15:00:33 -0600

    Users Model Enhancements

--- ScraperAdmin/DataAccess/Models/Users.cs
+++ ScraperAdmin/DataAccess/Models/Users.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ScraperAdmin.DataAccess.Models
 {
@@ -8,9 +9,9 @@ namespace ScraperAdmin.DataAccess.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
         public int UserId { get; set; }  // Mapea a la columna "UserId" en la base de datos
 
-        [Required]
         [Column("Email")]
         public string Email { get; set; }  // Mapea a la columna "Email"
 
@@ -18,12 +19,13 @@ namespace ScraperAdmin.DataAccess.Models
         [Column("Username")]
         public string Username { get; set; }  // Mapea a la columna "Username"
 
-        [Required]
+        [JsonIgnore]  // Evita que AccessToken sea expuesto en JSON
         [Column("AccessToken")]
-        public string AccessToken { get; set; }  // Mapea a la columna "AccessToken"
-        
+        public string? AccessToken { get; set; }  // Hacemos que AccessToken sea opcional en el modelo
+
         [Column("CreatedAt")]
+        [JsonIgnore]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]  // El valor de "CreatedAt" será gestionado por la base de datos
-        public DateTime CreatedAt { get; set; }  // Mapea a la columna "CreatedAt"     
+        public DateTime CreatedAt { get; set; }  // Mapea a la columna "CreatedAt"
     }
 }
