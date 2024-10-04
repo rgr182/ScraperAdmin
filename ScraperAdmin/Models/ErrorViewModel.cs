50ee443b9cc9d8bbda6a694e78135140ae37ebb4
RAULPC\rgr_1 <rgr182>
2024-10-03 19:58:59 -0600

    Quitando Basura

--- ScraperAdmin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ScraperAdmin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
