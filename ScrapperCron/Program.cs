c01455b38e3a69937aabae3e65f9b88d8e3c03f1
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-10-22 15:28:35 -0600

    elimine mensajes en español

--- ScrapperCron/Program.cs
+++ ScrapperCron/Program.cs
@@ -11,32 +11,23 @@ namespace Cron_BolsaDeTrabajo
     {
         static async Task Main(string[] args)
         {
-            // Crea el host que manejará tu aplicación como un servicio de Windows
             var host = Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((context, config) =>
                 {
-                    // Cargar la configuración de appsettings.cron.json
                     config.SetBasePath(AppContext.BaseDirectory);
                     config.AddJsonFile("appsettings.cron.json", optional: false, reloadOnChange: true);
                 })
                 .ConfigureServices((hostContext, services) =>
                 {
-                    // Configurar las dependencias
-                    var configuration = LoadConfiguration(); // Usa tu método existente para cargar la configuración
+                    var configuration = LoadConfiguration();
                     services.AddSingleton<IConfiguration>(configuration);
-                    services.AddScoped<ICronService, CronService>(); // Mantén la inyección de dependencia
-
-                    // Registrar el servicio hospedado que correrá el CronService
+                    services.AddScoped<ICronService, CronService>();
                     services.AddHostedService<CronHostedService>();
                 })
-                .UseWindowsService() // Esto indica que la aplicación será un servicio de Windows
+                .UseWindowsService()
                 .Build();
-
-            // Ejecutar el servicio
             await host.RunAsync();
         }
-
-        // Método para cargar la configuración desde appsettings.cron.json (esto es igual al original)
         private static IConfiguration LoadConfiguration()
         {
             var builder = new ConfigurationBuilder()
